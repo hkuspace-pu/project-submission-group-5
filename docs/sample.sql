56de8feb833cc9a02a779243601e524fcd259d18
Leo Hung <shing.hung@students.plymouth.ac.uk>
2023-05-04 16:10:18 +0800

    Restruct database and update sample sql

--- docs/sample.sql
+++ docs/sample.sql
@@ -1,83 +1,59 @@
--- Insert 10 rows into the Surveyors table
-INSERT INTO Surveyors (SurveyorID, Name, Email, Password)
-VALUES
-    (1, 'John Doe', 'johndoe@example.com', 'password1'),
-    (2, 'Jane Smith', 'janesmith@example.com', 'password2'),
-    (3, 'Bob Johnson', 'bjohnson@example.com', 'password3'),
-    (4, 'Samantha Lee', 'samanthalee@example.com', 'password4'),
-    (5, 'Michael Chen', 'michaelchen@example.com', 'password5'),
-    (6, 'Emily Davis', 'emilydavis@example.com', 'password6'),
-    (7, 'David Kim', 'davidkim@example.com', 'password7'),
-    (8, 'Karen Thompson', 'karenthompson@example.com', 'password8'),
-    (9, 'Peter Rodriguez', 'peterrodriguez@example.com', 'password9'),
-    (10, 'Lisa Martinez', 'lisamartinez@example.com', 'password10');
+INSERT INTO Species (SpeciesID, SpeciesCode, CommonName, SciName) VALUES
+  (1, 'SP001', 'American Goldfinch', 'Spinus tristis'),
+  (2, 'SP002', 'Bald Eagle', 'Haliaeetus leucocephalus'),
+  (3, 'SP003', 'Canada Goose', 'Branta canadensis'),
+  (4, 'SP004', 'Mallard Duck', 'Anas platyrhynchos'),
+  (5, 'SP005', 'Red-tailed Hawk', 'Buteo jamaicensis'),
+  (6, 'SP006', 'Ruby-throated Hummingbird', 'Archilochus colubris'),
+  (7, 'SP007', 'Wild Turkey', 'Meleagris gallopavo'),
+  (8, 'SP008', 'Woodpecker', 'Picidae'),
+  (9, 'SP009', 'Blue Jay', 'Cyanocitta cristata'),
+  (10, 'SP010', 'Northern Cardinal', 'Cardinalis cardinalis');
 
--- Insert 10 rows into the Species table
-INSERT INTO Species (SpeciesID, CommonName, LatinName)
-VALUES
-    (1, 'American Robin', 'Turdus migratorius'),
-    (2, 'Mallard', 'Anas platyrhynchos'),
-    (3, 'Eastern Gray Squirrel', 'Sciurus carolinensis'),
-    (4, 'Common Raven', 'Corvus corax'),
-    (5, 'Red-tailed Hawk', 'Buteo jamaicensis'),
-    (6, 'White-tailed Deer', 'Odocoileus virginianus'),
-    (7, 'Eastern Cottontail', 'Sylvilagus floridanus'),
-    (8, 'American Black Bear', 'Ursus americanus'),
-    (9, 'Bobcat', 'Lynx rufus'),
-    (10, 'Eastern Bluebird', 'Sialia sialis');
+INSERT INTO Users (UserID, PhotoUrl, Name, Email, Password, UserType) VALUES
+  (1, 'https://example.com/photos/user1.jpg', 'John Smith', 'john@example.com', 'password1', 'admin'),
+  (2, 'https://example.com/photos/user2.jpg', 'Mary Johnson', 'mary@example.com', 'password2', 'admin'),
+  (3, 'https://example.com/photos/user3.jpg', 'Tom Jones', 'tom@example.com', 'password3', 'expert'),
+  (4, 'https://example.com/photos/user4.jpg', 'Jane Doe', 'jane@example.com', 'password4', 'expert'),
+  (5, 'https://example.com/photos/user5.jpg', 'Bob Smith', 'bob@example.com', 'password5', 'moderator'),
+  (6, 'https://example.com/photos/user6.jpg', 'Alice Johnson', 'alice@example.com', 'password6', 'moderator'),
+  (7, 'https://example.com/photos/user7.jpg', 'Mike Jones', 'mike@example.com', 'password7', 'user'),
+  (8, 'https://example.com/photos/user8.jpg', 'Sue Doe', 'sue@example.com', 'password8', 'user'),
+  (9, 'https://example.com/photos/user9.jpg', 'Jim Smith', 'jim@example.com', 'password9', 'user'),
+  (10, 'https://example.com/photos/user10.jpg', 'Lisa Johnson', 'lisa@example.com', 'password10', 'user');
 
--- Insert data into the Records table
-INSERT INTO Records (RecordID, SurveyorID, SpeciesID, Longitude, Latitude, DateObserved)
-VALUES
-(1, 1, 1, -122.420, 37.775, '2022-04-01 12:30:00'),
-(2, 2, 2, -122.419, 37.776, '2022-04-02 14:45:00'),
-(3, 3, 3, -122.418, 37.777, '2022-04-03 16:00:00'),
-(4, 4, 4, -122.417, 37.778, '2022-04-04 10:15:00'),
-(5, 5, 5, -122.416, 37.779, '2022-04-05 11:30:00'),
-(6, 6, 6, -122.415, 37.780, '2022-04-06 09:45:00'),
-(7, 7, 7, -122.414, 37.781, '2022-04-07 08:00:00'),
-(8, 8, 8, -122.413, 37.782, '2022-04-08 13:30:00'),
-(9, 9, 9, -122.412, 37.783, '2022-04-09 12:45:00'),
-(10, 10, 10, -122.411, 37.784, '2022-04-10 11:00:00');
+INSERT INTO Records (RecordID, UserID, SpeciesID, Longitude, Latitude, DateObserved, Age, Sex, Location, PhotoUrl, Width, Height, Status, ReviewerID) VALUES 
+(1, 1, 1, -123.1207, 49.2827, '2023-04-28 09:00:00', 2, 2, 'Stanley Park', 'https://example.com/record1.jpg', 800, 600, 1, 3),
+(2, 2, 2, -122.3493, 47.6205, '2023-04-30 13:20:00', 3, 1, 'Discovery Park', 'https://example.com/record2.jpg', 1200, 800, 1, 4),
+(3, 3, 3, -122.4194, 37.7749, '2023-05-01 11:45:00', 1, 2, 'Golden Gate Park', 'https://example.com/record3.jpg', 900, 600, 0, 5),
+(4, 4, 4, -118.2437, 34.0522, '2023-04-27 14:30:00', 2, 1, 'Griffith Park', 'https://example.com/record4.jpg', 800, 600, 0, 6),
+(5, 5, 5, -122.3321, 47.6062, '2023-05-02 16:40:00', 3, 2, 'Seattle Waterfront', 'https://example.com/record5.jpg', 1200, 800, 1, 7),
+(6, 6, 6, -118.4120, 33.9416, '2023-04-28 10:15:00', 1, 1, 'El Matador Beach', 'https://example.com/record6.jpg', 900, 600, 1, 8),
+(7, 7, 7, -123.1207, 49.2827, '2023-05-03 08:30:00', 2, 2, 'Stanley Park', 'https://example.com/record7.jpg', 800, 600, 0, 9),
+(8, 8, 8, -122.3493, 47.6205, '2023-04-30 12:10:00', 3, 1, 'Discovery Park', 'https://example.com/record8.jpg', 1200, 800, 1, 10),
+(9, 9, 9, -122.4194, 37.7749, '2023-05-01 14:20:00', 1, 2, 'Golden Gate Park', 'https://example.com/record9.jpg', 900, 600, 0, 11),
+(10, 10, 10, -118.2437, 34.0522, '2023-04-27 16:50:00', 2, 1, 'Griffith Park', 'https://example.com/record10.jpg', 800, 600, 1, 12);
 
--- Insert data into the Users table
-INSERT INTO Users (UserID, Name, Email, Password, UserType)
-VALUES
-(1, 'John Doe', 'johndoe@example.com', 'password1', 'admin'),
-(2, 'Jane Smith', 'janesmith@example.com', 'password2', 'expert'),
-(3, 'Bob Johnson', 'bobjohnson@example.com', 'password3', 'moderator'),
-(4, 'Sarah Lee', 'sarahlee@example.com', 'password4', 'user'),
-(5, 'David Kim', 'davidkim@example.com', 'password5', 'user'),
-(6, 'Emily Brown', 'emilybrown@example.com', 'password6', 'user'),
-(7, 'Mike Davis', 'mikedavis@example.com', 'password7', 'user'),
-(8, 'Karen Wilson', 'karenwilson@example.com', 'password8', 'user'),
-(9, 'Tom Jones', 'tomjones@example.com', 'password9', 'user'),
-(10, 'Lisa Lee', 'lisalee@example.com', 'password10', 'user');
+INSERT INTO Comment (UserID, RecordID, Content, Rating) VALUES 
+(1, 1, 'Great sighting!', 5),
+(2, 2, 'Interesting observation, thanks for sharing!', 4),
+(3, 3, 'Love this photo!', 5),
+(4, 4, 'Nice record, keep up the good work!', 4),
+(5, 5, 'Wow, this is amazing!', 5),
+(6, 6, 'Thanks for sharing this sighting!', 4),
+(7, 7, 'I saw a similar bird in my garden last week', 3),
+(8, 8, 'Beautiful bird, great job capturing it on camera!', 5),
+(9, 9, 'This is so cool, I wish I could see this bird in person!', 5),
+(10, 10, 'Awesome record, thanks for sharing!', 4);
 
--- Insert sample data into UserRecords table
-INSERT INTO UserRecords (UserRecordID, UserID, SpeciesID, Longitude, Latitude, DateObserved)
-VALUES 
-(1, 2, 3, -73.935242, 40.730610, '2022-01-01 09:30:00'),
-(2, 3, 5, -118.243683, 34.052235, '2022-01-02 11:45:00'),
-(3, 1, 7, -84.388000, 33.749000, '2022-01-03 14:15:00'),
-(4, 4, 2, -0.127758, 51.507351, '2022-01-04 16:00:00'),
-(5, 1, 8, -75.165222, 39.952584, '2022-01-05 18:30:00'),
-(6, 2, 4, -118.326743, 34.098342, '2022-01-06 20:00:00'),
-(7, 3, 6, -87.629798, 41.878114, '2022-01-07 10:00:00'),
-(8, 4, 1, -73.992852, 40.719383, '2022-01-08 12:15:00'),
-(9, 1, 2, -122.419418, 37.774929, '2022-01-09 14:45:00'),
-(10, 2, 5, -84.512020, 39.103119, '2022-01-10 16:30:00');
-
--- Insert sample data into News table
-INSERT INTO News (NewsID, SpeciesID, Title, Content, PublishDate)
-VALUES
-(1, 3, 'New study finds decline in bird populations', 'A recent study has found a significant decline in bird populations across North America.', '2022-04-15 08:00:00'),
-(2, 7, 'Rare butterfly spotted in local park', 'A rare butterfly species, thought to be extinct in this area, was spotted by a group of park visitors last weekend.', '2022-05-02 12:30:00'),
-(3, 2, 'Conservation group launches new campaign', 'The Wildlife Conservation Alliance has launched a new campaign to raise awareness about the importance of protecting endangered species.', '2022-05-05 09:15:00'),
-(4, 6, 'New reptile species discovered in the Amazon', 'Scientists have discovered a new species of snake in the Amazon rainforest, which they believe may have medicinal properties.', '2022-05-10 16:45:00'),
-(5, 5, 'Global warming threatens penguin populations', 'A new report has warned that global warming could have devastating effects on penguin populations in Antarctica.', '2022-05-20 11:00:00'),
-(6, 1, 'Rare bird sighting at local nature reserve', 'A rare bird species, not seen in this area for over a decade, was spotted by a group of birdwatchers at the local nature reserve.', '2022-05-25 06:45:00'),
-(7, 4, 'New law proposed to protect endangered species', 'A group of lawmakers has proposed a new law to protect endangered species and their habitats, which has received widespread support from conservation groups.', '2022-06-01 14:20:00'),
-(8, 8, 'Cheetah population reaches new high', 'The latest census has revealed that the cheetah population in Africa has reached a new high, thanks to successful conservation efforts.', '2022-06-05 10:10:00'),
-(9, 9, 'Rare orchid found in local woodland', 'A rare orchid species, thought to be extinct in this area, was discovered by a group of botanists in a local woodland.', '2022-06-12 13:00:00'),
-(10, 2, 'New species of beetle discovered in Indonesia', 'Scientists have discovered a new species of beetle in the rainforests of Indonesia, which they believe could have important ecological roles.', '2022-06-20 09:30:00');
+INSERT INTO News (NewsID, SpeciesID, Title, Content, PhotoUrl, PublishDate) VALUES 
+(1, 1, 'New discovery of species', 'We have discovered a new species in the Amazon rainforest!', 'https://example.com/image.jpg', '2023-05-01'),
+(2, 2, 'Conservation efforts', 'Efforts to conserve the elephant population are underway.', 'https://example.com/image.jpg', '2023-04-29'),
+(3, 3, 'Polar bears in danger', 'The polar bear population is declining due to climate change.', 'https://example.com/image.jpg', '2023-04-28'),
+(4, 4, 'Giant squid spotted', 'A rare sighting of a giant squid has been reported off the coast of Japan.', 'https://example.com/image.jpg', '2023-04-27'),
+(5, 5, 'Endangered bird species', 'The population of the black-capped vireo is declining rapidly.', 'https://example.com/image.jpg', '2023-04-26'),
+(6, 6, 'New study on bee decline', 'A new study has shed light on the decline of bee populations.', 'https://example.com/image.jpg', '2023-04-25'),
+(7, 7, 'Illegal ivory trade', 'The illegal ivory trade is still a major problem despite efforts to combat it.', 'https://example.com/image.jpg', '2023-04-24'),
+(8, 8, 'Rare bird sighting', 'A rare sighting of a red-tailed hawk has been reported in Central Park.', 'https://example.com/image.jpg', '2023-04-23'),
+(9, 9, 'Marine plastic pollution', 'The amount of plastic in the ocean is reaching alarming levels.', 'https://example.com/image.jpg', '2023-04-22'),
+(10, 10, 'Biodiversity hotspots', 'Some areas of the world are home to an incredibly high number of species.', 'https://example.com/image.jpg', '2023-04-21');
--- docs/setup.sql
+++ docs/setup.sql
@@ -1,49 +1,40 @@
--- Create the Surveyors table
-CREATE TABLE Surveyors (
-  SurveyorID int PRIMARY KEY,
-  Name varchar(255) NOT NULL,
-  Email varchar(255) NOT NULL,
-  Password varchar(255) NOT NULL
-);
-
 -- Create the Species table
 CREATE TABLE Species (
   SpeciesID int PRIMARY KEY,
+  SpeciesCode varchar(255) NOT NULL,
   CommonName varchar(255) NOT NULL,
-  LatinName varchar(255) NOT NULL
+  SciName varchar(255) NOT NULL
 );
 
 -- Create the Records table
 CREATE TABLE Records (
   RecordID int PRIMARY KEY,
-  SurveyorID int NOT NULL,
-  SpeciesID int NOT NULL,
-  Longitude float NOT NULL,
-  Latitude float NOT NULL,
-  DateObserved datetime NOT NULL,
-  CONSTRAINT FK_Surveyor FOREIGN KEY (SurveyorID) REFERENCES Surveyors(SurveyorID),
-  CONSTRAINT FK_Species FOREIGN KEY (SpeciesID) REFERENCES Species(SpeciesID)
-);
-
--- Create the Users table
-CREATE TABLE Users (
-  UserID int PRIMARY KEY,
-  Name varchar(255) NOT NULL,
-  Email varchar(255) NOT NULL,
-  Password varchar(255) NOT NULL,
-  UserType varchar(50) NOT NULL
-);
-
--- Create the UserRecords table
-CREATE TABLE UserRecords (
-  UserRecordID int PRIMARY KEY,
   UserID int NOT NULL,
   SpeciesID int NOT NULL,
   Longitude float NOT NULL,
   Latitude float NOT NULL,
   DateObserved datetime NOT NULL,
-  CONSTRAINT FK_User FOREIGN KEY (UserID) REFERENCES Users(UserID),
-  CONSTRAINT FK_Species2 FOREIGN KEY (SpeciesID) REFERENCES Species(SpeciesID)
+  Age int NOT NULL,
+  Sex int NOT NULL,
+  Location varchar(255) NOT NULL,
+  PhotoUrl varchar(255) NOT NULL,
+  Width int NOT NULL,
+  Height int NOT NULL,
+  Status int NOT NULL,
+  ReviewerID int NOT NULL,
+  CONSTRAINT fk_user FOREIGN KEY (UserID) REFERENCES Users(UserID),
+  CONSTRAINT fk_reviewer_id FOREIGN KEY (UserID) REFERENCES Users(UserID),
+  CONSTRAINT fk_species FOREIGN KEY (SpeciesID) REFERENCES Species(SpeciesID)
+);
+
+-- Create the Users table
+CREATE TABLE Users (
+  UserID int PRIMARY KEY,
+  PhotoUrl varchar(255) NOT NULL,
+  Name varchar(255) NOT NULL,
+  Email varchar(255) NOT NULL,
+  Password varchar(255) NOT NULL,
+  UserType varchar(50) NOT NULL
 );
 
 -- Create the News table
@@ -52,95 +43,20 @@ CREATE TABLE News (
   SpeciesID int NOT NULL,
   Title varchar(255) NOT NULL,
   Content text NOT NULL,
+  PhotoUrl varchar(255) NOT NULL,
   PublishDate datetime NOT NULL,
-  CONSTRAINT FK_Species3 FOREIGN KEY (SpeciesID) REFERENCES Species(SpeciesID)
+  CONSTRAINT fk_species_2 FOREIGN KEY (SpeciesID) REFERENCES Species(SpeciesID)
 );
 
-
-/************************* VIEW *************************/
-
--- -- Create VIEW AllRecords table
--- CREATE VIEW AllRecords AS
--- SELECT Records.RecordID, Surveyors.Name AS SurveyorName, Species.CommonName, Species.LatinName, Records.Longitude, Records.Latitude, Records.DateObserved
--- FROM Records
--- INNER JOIN Surveyors ON Records.SurveyorID = Surveyors.SurveyorID
--- INNER JOIN Species ON Records.SpeciesID = Species.SpeciesID;
-
--- -- Create VIEW AllUserRecords table
--- CREATE VIEW AllUserRecords AS
--- SELECT UserRecords.UserRecordID, Users.Name AS UserName, Species.CommonName, UserRecords.Longitude, UserRecords.Latitude, UserRecords.DateObserved
--- FROM UserRecords
--- INNER JOIN Users ON UserRecords.UserID = Users.UserID
--- INNER JOIN Species ON UserRecords.SpeciesID = Species.SpeciesID;
-
--- -- Create VIEW AllNews table
--- CREATE VIEW AllNews AS
--- SELECT News.NewsID, Species.CommonName, Species.LatinName, News.Title, News.Content, News.PublishDate
--- FROM News
--- INNER JOIN Species ON News.SpeciesID = Species.SpeciesID;
-
--- /************************* Trigger *************************/
-
--- -- Trigger to insert a new record in the UserRecords table when a new record is added to the Records table by a general user
--- CREATE TRIGGER InsertUserRecord
--- ON Records
--- AFTER INSERT
--- AS
--- BEGIN
---     DECLARE @UserID INT, @SpeciesID INT, @Longitude FLOAT, @Latitude FLOAT, @DateObserved DATETIME
-
---     SELECT @UserID = UserID, @SpeciesID = SpeciesID, @Longitude = Longitude, @Latitude = Latitude, @DateObserved = DateObserved
---     FROM inserted
---     WHERE SurveyorID IS NULL -- Only trigger for records added by general users
-
---     IF (@UserID IS NOT NULL AND @SpeciesID IS NOT NULL AND @Longitude IS NOT NULL AND @Latitude IS NOT NULL AND @DateObserved IS NOT NULL)
---     BEGIN
---         INSERT INTO UserRecords (UserID, SpeciesID, Longitude, Latitude, DateObserved)
---         VALUES (@UserID, @SpeciesID, @Longitude, @Latitude, @DateObserved)
---     END
--- END
-
--- -- Trigger to update the News table when a new record is added to the Records table
--- CREATE TRIGGER UpdateNews
--- ON Records
--- AFTER INSERT
--- AS
--- BEGIN
---     DECLARE @SpeciesID INT, @CommonName VARCHAR(255), @LatinName VARCHAR(255), @RecordCount INT
-
---     SELECT @SpeciesID = SpeciesID
---     FROM inserted
-
---     IF (@SpeciesID IS NOT NULL)
---     BEGIN
---         SELECT @CommonName = CommonName, @LatinName = LatinName
---         FROM Species
---         WHERE SpeciesID = @SpeciesID
-
---         SELECT @RecordCount = COUNT(*)
---         FROM Records
---         WHERE SpeciesID = @SpeciesID
-
---         UPDATE News
---         SET Title = 'New record for ' + @CommonName,
---             Content = 'A new record has been added for ' + @CommonName + ' (' + @LatinName + '). There are now ' + CONVERT(VARCHAR, @RecordCount) + ' records for this species in the database.',
---             PublishDate = GETDATE()
---         WHERE SpeciesID = @SpeciesID
---     END
--- END
-
--- -- Trigger to prevent deletion of surveyors that have records in the Records table
--- CREATE TRIGGER PreventSurveyorDeletion
--- ON Surveyors
--- INSTEAD OF DELETE
--- AS
--- BEGIN
---     IF EXISTS (SELECT * FROM deleted d INNER JOIN Records r ON d.SurveyorID = r.SurveyorID)
---     BEGIN
---         PRINT 'This surveyor cannot be deleted because they have records in the database.'
---     END
---     ELSE
---     BEGIN
---         DELETE FROM Surveyors WHERE SurveyorID IN (SELECT SurveyorID FROM deleted)
---     END
--- END
+-- Create the Comment table
+CREATE TABLE Comment (
+  CommentID INT IDENTITY PRIMARY KEY,
+  UserID INT NOT NULL,
+  RecordID INT NOT NULL,
+  Content VARCHAR(2048),
+  Rating INT DEFAULT 0,
+  UpdatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_user_2 FOREIGN KEY (UserID) REFERENCES Users(UserID),
+  CONSTRAINT fk_record_id FOREIGN KEY (RecordID) REFERENCES Records(RecordID),
+);
